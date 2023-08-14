c5603b4aee47b1453ff69f6e43a11ed94bb04b53
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-08-14 01:05:38 -0500

    feat: add auth mutations and queries

---
+++ src/graphql/mutations/auth.ts
@@ -0,0 +1,28 @@
+import { gql, TypedDocumentNode } from '@apollo/client';
+import { EmailAuthVariabes, GoogleAuthVariables } from 'src/graphql/types/auth';
+import { UserData } from 'src/graphql/types/user';
+import { USER_FRAGMENT } from 'src/graphql/fragments';
+
+export const SIGN_UP_WITH_EMAIL: TypedDocumentNode<UserData, EmailAuthVariabes> = gql`
+  ${USER_FRAGMENT}
+  mutation signUpWithEmail($signUpInput: CreateUserInput!) {
+    signUpWithEmail(signUpInput: $signUpInput) {
+      ...User
+    }
+  }
+`;
+
+export const SIGN_UP_WITH_GOOGLE: TypedDocumentNode<UserData, GoogleAuthVariables> = gql`
+  ${USER_FRAGMENT}
+  mutation signUpWithEmail($tokenInput: TokenInput!) {
+    signUpWithGoogle(googleToken: $tokenInput) {
+      ...User
+    }
+  }
+`;
+
+export const GET_NEW_REFRESH_TOKEN = gql`
+  mutation getNewRefreshToken($userdId: String!) {
+    getNewRefreshToken(userId: $userId)
+  }
+`;
---
+++ src/graphql/queries/auth.ts
@@ -0,0 +1,28 @@
+import { gql, TypedDocumentNode } from '@apollo/client';
+import { USER_FRAGMENT } from 'src/graphql/fragments';
+import { EmailAuthVariabes, GoogleAuthVariables } from 'src/graphql/types/auth';
+import { UserData } from 'src/graphql/types/user';
+
+export const SIGN_IN_WITH_EMAIL: TypedDocumentNode<UserData, EmailAuthVariabes> = gql`
+  ${USER_FRAGMENT}
+  query signInWithEmail($signInInput: GetUserInput!) {
+    signInWithEmail(signInInput: $signInInput) {
+      ...User
+    }
+  }
+`;
+
+export const SIGN_IN_WITH_GOOGLE: TypedDocumentNode<UserData, GoogleAuthVariables> = gql`
+  ${USER_FRAGMENT}
+  query signInWithGoogle($tokenInput: TokenInput!) {
+    signInWithGoogle(googleToken: $tokenInput) {
+      ...User
+    }
+  }
+`;
+
+export const GET_NEW_ACCESS_TOKEN = gql`
+  query getNewAccessToken($userId: String!) {
+    getNewAccessToken(userId: $userId)
+  }
+`;
