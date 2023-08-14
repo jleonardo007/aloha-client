4ebf9c90d69ac9b71dcc66dfe6da88c33fc094e6
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-08-14 01:04:17 -0500

    feat: add google clientId variable to env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_GRAPHQL_SERVER_URL: string;
-  // more env variables...
+  readonly VITE_GOOGLE_CLIENT_ID: string;
 }
 
 interface ImportMeta {
