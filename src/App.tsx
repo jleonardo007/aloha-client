75428e9ed684112984a5d5b89467c1c5f3607dfa
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-08-14 01:15:13 -0500

    feat: add auth component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
+import { useState } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import Auth from 'src/components/auth';
+import useCurrentUser from 'src/hooks/current-user';
+import { GOOGLE_CLIENT_ID } from 'src/constants/auth';
+
 function App() {
+  const { CurrentUserContext, currentUser, getCurrentUser, getCurrentUserFromGoogle } =
+    useCurrentUser();
+  const [width, setWidth] = useState<number | undefined>(0);
+
+  if (currentUser) {
+    return (
+      <CurrentUserContext.Provider value={currentUser}>
+        <h1>UI</h1>
+      </CurrentUserContext.Provider>
+    );
+  }
+
   return (
-    <div>
-      <h1>Initial UI</h1>
-    </div>
+    <GoogleOAuthProvider
+      clientId={GOOGLE_CLIENT_ID}
+      onScriptLoadSuccess={() => {
+        if (document.querySelector<HTMLDivElement>('.input')) {
+          setWidth(document.querySelector<HTMLDivElement>('.input')?.offsetWidth);
+        }
+      }}
+    >
+      <Auth
+        getCurrentUser={getCurrentUser}
+        getCurrentUserFromGoogle={getCurrentUserFromGoogle}
+        widthForGoogleButton={width}
+      />
+    </GoogleOAuthProvider>
   );
 }
 
