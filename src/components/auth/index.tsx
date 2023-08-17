cd70d7bf34519178cf8ffbab18b1aa946e90894a
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-08-17 01:15:15 -0500

    feat: add auth form validation

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -1,7 +1,20 @@
 import { useState, FormEvent } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
-import { AUTH_TEXTS, INPUTS, BUTTONS, GOOGLE_BUTTON } from 'src/constants/auth';
-import { AuthParentProps, AuthFormProps, AuthLinksProps, Credentials } from 'src/types/auth';
+import {
+  AUTH_TEXTS,
+  INPUTS,
+  BUTTONS,
+  GOOGLE_BUTTON,
+  AUTH_VALIDATION,
+  ERROR_MESSAGES,
+} from 'src/constants/auth';
+import {
+  AuthParentProps,
+  AuthFormProps,
+  AuthLinksProps,
+  Credentials,
+  UserCredentials,
+} from 'src/types/auth';
 import Input from 'src/components/common/input';
 import Button from 'src/components/common/button';
 import loginBackground from 'src/resources/images/aloha-login-bg.svg';
@@ -23,7 +36,60 @@ function AuthLinks({ text, linkText, onClick }: AuthLinksProps) {
 }
 
 function AuthForm({ signUpForm, children, getCurrentUser, toggleForm }: AuthFormProps) {
-  function submitHandler(event: FormEvent<HTMLFormElement>, isSignUp: boolean) {
+  const [errorMessages, setErrorMessages] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+  });
+
+  function validateCredentials({ fullName, email, password }: UserCredentials) {
+    let result = {
+      fullName: '',
+      email: '',
+      password: '',
+      isValid: false,
+    };
+
+    if (signUpForm) {
+      if (fullName) {
+        if (fullName.length > AUTH_VALIDATION.fullNameMaxLength) {
+          result = { ...result, fullName: ERROR_MESSAGES.fullName };
+        } else {
+          result = { ...result, fullName: '' };
+        }
+      } else {
+        result = { ...result, fullName: ERROR_MESSAGES.required };
+      }
+    }
+
+    if (password) {
+      if (password.length < AUTH_VALIDATION.passwordMinLength) {
+        result = { ...result, password: ERROR_MESSAGES.password };
+      } else {
+        result = { ...result, password: '' };
+      }
+    } else {
+      result = { ...result, password: ERROR_MESSAGES.required };
+    }
+
+    if (email) {
+      if (!email.match(AUTH_VALIDATION.validEmail)) {
+        result = { ...result, email: ERROR_MESSAGES.email };
+      } else {
+        result = { ...result, email: '' };
+      }
+    } else {
+      result = { ...result, email: ERROR_MESSAGES.required };
+    }
+
+    if (!result.email && !result.fullName && !result.password) {
+      result = { ...result, isValid: true };
+    }
+
+    return result;
+  }
+
+  function submitHandler(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     const userCredentials: Credentials = {
@@ -37,11 +103,16 @@ function AuthForm({ signUpForm, children, getCurrentUser, toggleForm }: AuthForm
       userCredentials[key] = value;
     }
 
-    getCurrentUser(userCredentials, isSignUp);
+    const { isValid, ...result } = validateCredentials(userCredentials);
+    setErrorMessages(result);
+
+    if (isValid) {
+      getCurrentUser(userCredentials, signUpForm);
+    }
   }
 
   return (
-    <form className="h-full pt-4" onSubmit={(event) => submitHandler(event, signUpForm)}>
+    <form className="h-full pt-4" onSubmit={submitHandler} noValidate>
       <h1 className="mb-4 ml-8 text-4xl text-gray-700 capitalize">{AUTH_TEXTS.mainText}</h1>
       {signUpForm && (
         <div className="w-4/5 mx-auto mb-2">
@@ -51,6 +122,7 @@ function AuthForm({ signUpForm, children, getCurrentUser, toggleForm }: AuthForm
             inputName={INPUTS.fullName.inputName}
             placeholder={INPUTS.fullName.placeholder}
             required={INPUTS.fullName.required}
+            errorMessage={errorMessages.fullName}
           />
         </div>
       )}
@@ -61,6 +133,7 @@ function AuthForm({ signUpForm, children, getCurrentUser, toggleForm }: AuthForm
           inputName={INPUTS.email.inputName}
           placeholder={INPUTS.email.placeholder}
           required={INPUTS.email.required}
+          errorMessage={errorMessages.email}
         />
       </div>
       <div className="w-4/5 mx-auto mb-6">
@@ -70,6 +143,7 @@ function AuthForm({ signUpForm, children, getCurrentUser, toggleForm }: AuthForm
           inputName={INPUTS.password.inputName}
           placeholder={INPUTS.password.placeholder}
           required={INPUTS.password.required}
+          errorMessage={errorMessages.password}
         />
       </div>
       <div className="w-4/5 h-10 mx-auto mb-3">
