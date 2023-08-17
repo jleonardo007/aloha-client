7e5c563e1fcd7f0139673a21b067807715eb4bd4
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-08-17 01:50:49 -0500

    feat: add spinner to button

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,24 +1,28 @@
+import Spinner from 'src/components/svg-icons/spinner';
+
 type ButtonProps = {
   type: 'button' | 'submit';
   label: string;
   disabled?: boolean;
-  spinner?: string;
   isLoading?: boolean;
   onClick?: () => void;
 };
 
-function Button({ type, label, disabled, spinner, isLoading, onClick }: ButtonProps) {
+const loadingLabel = 'Processing...';
+
+function Button({ type, label, disabled, isLoading, onClick }: ButtonProps) {
   if (isLoading) {
     return (
-      <button disabled>
-        <span>{spinner}</span>
+      <button className="w-full h-full capitalize rounded-[4px] bg-green-500 text-white" disabled>
+        <Spinner className="h-5 w-5 inline-block mr-3" />
+        {loadingLabel}
       </button>
     );
   }
 
   return (
     <button
-      className="w-full h-full  capitalize rounded-[4px] bg-green-500 text-white"
+      className="w-full h-full capitalize rounded-[4px] bg-green-500 text-white"
       type={type}
       disabled={disabled}
       onClick={onClick}
