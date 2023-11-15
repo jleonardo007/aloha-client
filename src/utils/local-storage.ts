095aa1764a8ec71365d578e99fd220e7a1967d16
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-11-15 00:03:05 -0500

    feat: add validation to run saveUser once

--- src/utils/local-storage.ts
+++ src/utils/local-storage.ts
@@ -2,6 +2,8 @@ import { AUTH_STORAGE } from 'src/constants/auth';
 import { UserData } from 'src/graphql/types/user';
 
 export function saveUser(userData: UserData) {
+  if (localStorage.user_data) return;
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { accessToken, ...user } = userData;
   localStorage.setItem(AUTH_STORAGE, JSON.stringify(user));
