ae56d8e0656c5eab27d1047dcb7929780beb6569
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-11-16 00:37:06 -0500

    feat: create no data view component

---
+++ src/components/no-data-view/index.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import { TransitionContext, TransitionsOptions } from 'src/context/app-transition';
+import noMessagesIcon from 'src/resources/icons/no-messages.svg';
+import noCalls from 'src/resources/icons/no-calls.svg';
+
+export default function NoDataView() {
+  const { currentTransition } = useContext(TransitionContext);
+
+  return (
+    <div className="h-[calc(100vh-88px)] bg-slate-100 pt-32">
+      {currentTransition === TransitionsOptions.chats && (
+        <img src={noMessagesIcon} className="w-20 h-20 mx-auto mb-4" alt="no messages" />
+      )}
+      {currentTransition === TransitionsOptions.calls && (
+        <img src={noCalls} className="w-16 h-16 mx-auto mb-4" alt="no calls" />
+      )}
+      <span className="capitalize text-2xl text-center block">{`don't have ${
+        currentTransition === TransitionsOptions.chats ? 'messages' : 'calls'
+      } yet`}</span>
+    </div>
+  );
+}
