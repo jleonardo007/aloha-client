39a355e89a1a1001466926c44a231d9774a70d37
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-11-16 00:28:31 -0500

    feat: creta app transition hook

---
+++ src/hooks/app-transtions.ts
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { TransitionContext, TransitionsOptions } from 'src/context/app-transition';
+
+export default function useAppTransitions() {
+  const [currentTransition, setCurrentTransition] = useState(TransitionsOptions.chats);
+  const [prevTransition, setPrevTransition] = useState(TransitionsOptions.chats);
+
+  function changeTransition(transition: TransitionsOptions, prevTransition: TransitionsOptions) {
+    setCurrentTransition(transition);
+    setPrevTransition(prevTransition);
+  }
+
+  return {
+    currentTransition,
+    prevTransition,
+    changeTransition,
+    TransitionContext,
+  };
+}
