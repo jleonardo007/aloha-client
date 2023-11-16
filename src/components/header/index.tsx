8ff40b2d949227cb378b0c56624226017660e1b2
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-11-15 23:52:45 -0500

    feat: create app header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import { CurrentUserContext } from 'src/context/current-user';
+import Menu from 'src/components/menu';
+import defaultAvatar from 'src/resources/images/default-avatar.svg';
+
+const APP_TITLE = 'Aloha Chat';
+
+export default function Header() {
+  const { profilePicture, fullName } = useContext(CurrentUserContext);
+
+  return (
+    <header className="h-12 px-3 flex items-center bg-teal-700">
+      <h1 className="grow text-xl text-slate-100">{APP_TITLE}</h1>
+      <img
+        className="w-9 h-9 rounded-full"
+        src={profilePicture ? profilePicture : defaultAvatar}
+        alt={`${fullName} avatar`}
+      />
+      <Menu />
+    </header>
+  );
+}
