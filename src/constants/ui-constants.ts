84c0c829a69de31a8f17006df4ae08ca0ab37bfe
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-11-18 23:26:10 -0500

    feat: create UI constants

---
+++ src/constants/ui-constants.ts
@@ -0,0 +1,20 @@
+const APP_TITLE = 'Aloha Chat';
+
+const TITLES_MESSAGING_TABS = {
+  chats: 'Chats',
+  calls: 'Calls',
+};
+
+const TITLES_CREATE_NEW = {
+  callLink: 'New call link',
+  newContact: 'New contact',
+  newGroup: 'New group',
+};
+
+const TEXT_NODATA_VIEW = {
+  messages: 'no messages',
+  calls: 'no calls',
+  contacts: 'no contacts',
+};
+
+export { APP_TITLE, TITLES_MESSAGING_TABS, TITLES_CREATE_NEW, TEXT_NODATA_VIEW };
