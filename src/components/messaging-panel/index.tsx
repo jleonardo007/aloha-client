af55e4afe3510adcd38dff1e485dcacd9e20ac2c
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-11-19 01:46:13 -0500

    feat: add reusable header to messaging panel

--- src/components/messaging-panel/index.tsx
+++ src/components/messaging-panel/index.tsx
@@ -1,12 +1,17 @@
 import { useContext } from 'react';
+import { CurrentUserContext } from 'src/context/current-user';
 import { TransitionContext, TransitionsOptions } from 'src/context/app-transition';
 import Header from 'src/components/header';
 import Messaging from 'src/components/messaging';
 import AddButton from 'src/components/create-button';
 import ContactsPanel from 'src/components/contacts-panel';
+import Menu from 'src/components/menu';
+import { APP_TITLE } from 'src/constants/ui-constants';
+import defaultAvatar from 'src/resources/images/default-avatar.svg';
 
 export default function MessagingPanel() {
   const { currentTransition } = useContext(TransitionContext);
+  const { profilePicture, fullName } = useContext(CurrentUserContext);
 
   if (currentTransition === TransitionsOptions.contacts) {
     return <ContactsPanel />;
@@ -14,7 +19,17 @@ export default function MessagingPanel() {
 
   return (
     <>
-      <Header />
+      <Header
+        title={APP_TITLE}
+        menu={<Menu />}
+        slot={
+          <img
+            className="w-9 h-9 rounded-full"
+            src={profilePicture ? profilePicture : defaultAvatar}
+            alt={`${fullName} avatar`}
+          />
+        }
+      />
       <Messaging />
       <AddButton />
     </>
