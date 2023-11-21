533cb6a38145c829aeefffaa3bc005cd114a365f
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-11-20 21:35:02 -0500

    feat: create back and search buttons

---
+++ src/components/common/search-button.tsx
@@ -0,0 +1,10 @@
+import Button from 'src/components/common/button';
+import searchIcon from 'src/resources/icons/search-icon.svg';
+
+type SearchButtonProps = {
+  enableSearch: () => void;
+};
+
+export default function SearchButton({ enableSearch }: SearchButtonProps) {
+  return <Button type="button" className="h-6 w-6 mr-2" icon={searchIcon} onClick={enableSearch} />;
+}
