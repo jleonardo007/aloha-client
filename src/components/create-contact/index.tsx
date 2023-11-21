e305a25f1ef36e2b567fa56d8ab5e4492f8f751a
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-11-20 23:53:48 -0500

    feat: create new contact form

---
+++ src/components/create-contact/index.tsx
@@ -0,0 +1,36 @@
+import { CreateContactProps } from 'src/types/contacts-panel';
+import { CREATE_CONTACT } from 'src/constants/ui-constants';
+import Header from 'src/components/header';
+import BackButton from 'src/components/common/back-button';
+import Input from 'src/components/common/input';
+import Button from 'src/components/common/button';
+
+export default function CreateContact({ setNoAction }: CreateContactProps) {
+  return (
+    <section className="h-screen">
+      <Header title={CREATE_CONTACT.headerTitle} backButton={<BackButton back={setNoAction} />} />
+      <form className="min-h-[calc(100vh-48px)] px-10 pt-10 relative">
+        <Input
+          type={CREATE_CONTACT.nameInput.type}
+          label={CREATE_CONTACT.nameInput.label}
+          inputName={CREATE_CONTACT.nameInput.inputName}
+          placeholder={CREATE_CONTACT.nameInput.placeholder}
+          required={CREATE_CONTACT.nameInput.required}
+        />
+        <div className="mb-4"></div>
+        <Input
+          type={CREATE_CONTACT.emailInput.type}
+          label={CREATE_CONTACT.emailInput.label}
+          inputName={CREATE_CONTACT.emailInput.inputName}
+          placeholder={CREATE_CONTACT.emailInput.placeholder}
+          required={CREATE_CONTACT.emailInput.required}
+        />
+        <Button
+          type="button"
+          label={CREATE_CONTACT.saveButton.label}
+          className="w-4/5 h-12 absolute top-[90%] rounded-3xl text-white bg-teal-700"
+        />
+      </form>
+    </section>
+  );
+}
