81f2a24624457572c7bd83c61edf0443234d8447
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-11-20 21:37:08 -0500

    feat: add back and search buttons to contacts panel

--- src/components/contacts-panel/index.tsx
+++ src/components/contacts-panel/index.tsx
@@ -5,10 +5,9 @@ import Header from 'src/components/header';
 import CreateNew from 'src/components/create-new';
 import ContactsList from 'src/components/contacts-list';
 import Menu from 'src/components/menu';
-import Button from 'src/components/common/button';
+import BackButton from 'src/components/common/back-button';
+import SearchButton from 'src/components/common/search-button';
 import { CONTACTS_PANEL_HEADER } from 'src/constants/ui-constants';
-import backIcon from 'src/resources/icons/arrow-left.svg';
-import searchIcon from 'src/resources/icons/search-icon.svg';
 
 export default function ContactsPanel() {
   const { prevTransition, changeTransition } = useContext(TransitionContext);
@@ -24,10 +23,8 @@ export default function ContactsPanel() {
         title={CONTACTS_PANEL_HEADER.title}
         subtitle={CONTACTS_PANEL_HEADER.contactsAmount(contacts.length)}
         menu={<Menu />}
-        slot={<Button type="button" icon={searchIcon} className="h-6 w-6 mr-2" />}
-        backButton={
-          <Button type="button" icon={backIcon} className="h-8 w-8 mr-2" onClick={goToPrevScreen} />
-        }
+        slot={<SearchButton enableSearch={() => {}} />}
+        backButton={<BackButton back={goToPrevScreen} />}
       />
       <CreateNew />
       <ContactsList />
