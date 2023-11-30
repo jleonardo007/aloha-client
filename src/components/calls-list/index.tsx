9372507d500f26d1ddb332e43e4ebdf477438fff
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-11-30 00:28:07 -0500

    chore: use calls array from user context

--- src/components/calls-list/index.tsx
+++ src/components/calls-list/index.tsx
@@ -4,9 +4,9 @@ import { CurrentUserContext } from 'src/context/current-user';
 import NoDataView from 'src/components/no-data-view';
 
 export default function CallsList() {
-  const { contacts } = useContext(CurrentUserContext);
+  const { calls } = useContext(CurrentUserContext);
 
-  if (contacts.length === 0) {
+  if (calls.length === 0) {
     return <NoDataView />;
   }
   return <div>CallsList</div>;
