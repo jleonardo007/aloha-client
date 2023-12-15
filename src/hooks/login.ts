4beb6abab9df115450b2bad6f8b683988a7d43a8
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2023-12-15 01:56:57 -0500

    chore: rename hook to useLogin

--- src/hooks/login.ts
+++ src/hooks/login.ts
@@ -1,15 +1,18 @@
+import { useContext, useEffect } from 'react';
+import { CurrentUserContext } from 'src/context/current-user';
+import { UserCredentials } from 'src/types/auth';
+import { UserData } from 'src/graphql/types/user';
+import { saveUser } from 'src/utils/local-storage';
+import { CurrentUserActions } from 'src/reducers/current-user';
 import {
   useSignInWithEmail,
   useSignUpWithEmail,
   useSignInWithGoogle,
   useSignUpWithGoogle,
 } from 'src/service-hooks/auth';
-import { CurrentUserContext } from 'src/context/current-user';
-import { UserCredentials } from 'src/types/auth';
-import { UserData } from 'src/graphql/types/user';
-import { readUser, saveUser } from 'src/utils/local-storage';
 
-export default function useCurrentUser() {
+export function useLogin() {
+  const { dispatch } = useContext(CurrentUserContext);
   const { signInWithEmail, signInData, isSignInLoading, signInError } = useSignInWithEmail();
   const { signUpWithEmail, signUpData, isSignUpLoading, signUpError } = useSignUpWithEmail();
   const { signInWithGoogle, googleSignInData, isGoogleSignInLoading, googleSignInError } =
@@ -17,15 +20,24 @@ export default function useCurrentUser() {
   const { signUpWithGoogle, googleSignUpData, isGoogleSignUpLoading, googleSignUpError } =
     useSignUpWithGoogle();
 
-  const currentUser: UserData | null | undefined =
-    readUser() || signInData || signUpData || googleSignInData || googleSignUpData;
+  const userData: UserData | null | undefined =
+    signInData || signUpData || googleSignInData || googleSignUpData;
   const isLoading =
     isSignInLoading || isSignUpLoading || isGoogleSignInLoading || isGoogleSignUpLoading;
   const error = signInError || signUpError || googleSignInError || googleSignUpError;
 
-  if (currentUser) {
-    saveUser(currentUser);
-  }
+  useEffect(() => {
+    if (!userData) {
+      return;
+    }
+
+    saveUser(userData);
+    dispatch({
+      type: CurrentUserActions.GET_USER_FROM_LOGIN,
+      payload: userData,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userData]);
 
   function getCurrentUser(userCredentials: UserCredentials, isSignUp: boolean) {
     if (isSignUp) {
@@ -58,8 +70,6 @@ export default function useCurrentUser() {
   }
 
   return {
-    CurrentUserContext,
-    currentUser,
     isLoading,
     error,
     getCurrentUser,
