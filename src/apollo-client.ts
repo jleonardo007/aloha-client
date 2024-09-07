57c1da6c3962ddd6bde635aa6e5f0f943ffcfbd0
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2024-09-07 03:23:04 -0500

    chore: allow send cookies from apollo client

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,6 +1,11 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+const link = createHttpLink({
+  uri: import.meta.env.VITE_GRAPHQL_SERVER_URL,
+  credentials: 'include',
+});
 
 export const apolloClient = new ApolloClient({
-  uri: import.meta.env.VITE_GRAPHQL_SERVER_URL,
   cache: new InMemoryCache(),
+  link,
 });
